e091cc4aca6c7335ce21ad9e99dbfcd35dd0ba56
morenm14 <morenm14@gmatc.matc.edu>
2021-03-17 13:31:17 -0500

    Activity 9 - TextViewApp Completed

---
+++ TextViewApp/TextViewApp/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  TextViewApp
+//
+//  Created by Mario Moreno on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var buttonObject: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.dataDetectorTypes = UIDataDetectorTypes.link
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.text = "this is an example of clickable text www.yahoo.com"
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification){
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue{
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification){
+        if((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil{
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y = 0
+            }
+        }
+    }
+
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if textView.isEditable == true{
+            textView.isEditable = false
+            textView.backgroundColor = UIColor.yellow
+            textView.textColor = UIColor.blue
+            textView.font = UIFont(name: "Courier", size: 24)
+        }else{
+            textView.isEditable = true;
+            textView.backgroundColor = UIColor.blue
+            textView.textColor = UIColor.white
+            textView.font = UIFont(name: "Ariel", size: 10)
+        }
+    }
+}
+
